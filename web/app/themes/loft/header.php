fcdc3dc16f4d001a31922f3c0ec3ab8a38d0751b
sergey <serger777@gmail.com>
2018-09-29 10:38:08 +0300

    local

---
+++ web/app/themes/loft/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <title><?= wp_title() ?></title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="stylesheet" href="<?php bloginfo("template_directory"); ?>/css/libs.min.css">
+    <link rel="stylesheet" href="<?php bloginfo("template_directory"); ?>/css/main.css">
+    <link rel="stylesheet" href="<?php bloginfo("template_directory"); ?>/css/media.css">
+    <?= wp_head() ?>
+</head>
+<body>
+<div class="wrapper">
+    <header class="main-header">
+        <div class="top-header">
+            <div class="top-header__wrap">
+                <div class="logotype-block">
+                    <div class="logo-wrap"><a href="/">run-ski.ru</a></div>
+                </div>
+                <nav class="main-navigation">
+                    <ul class="nav-list">
+                        <?
+
+                        wp_nav_menu(array(
+                            'menu' => 'header_menu',
+                            'menu_class' => 'menu1',
+                        ));
+                        ?>
+                    </ul>
+
+                </nav>
+            </div>
+        </div>
+
+    </header>
+    <!-- header_end-->
