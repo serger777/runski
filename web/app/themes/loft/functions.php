08e3cf3de687645c12a150d6108864a49b14b4db
sergey <serger777@gmail.com>
2018-12-16 15:54:31 +0300

    menu

--- web/app/themes/loft/functions.php
+++ web/app/themes/loft/functions.php
@@ -152,7 +152,12 @@ function title($limit)
 }
 
 
-
+function wph_cut_by_words($maxlen, $text) {
+    $len = (mb_strlen($text) > $maxlen)? mb_strripos(mb_substr($text, 0, $maxlen), ' ') : $maxlen;
+    $cutStr = mb_substr($text, 0, $len);
+    $temp = (mb_strlen($text) > $maxlen)? $cutStr. '...' : $cutStr;
+    return $temp;
+}
 
 function add_meta_tags() {
 
@@ -164,14 +169,11 @@ function add_meta_tags() {
 
         $meta = strip_shortcodes( $post->post_content );
 
-        $meta = str_replace( array("\n", "\r", "\t"), ' ', $meta );
+        $meta = str_replace( array("\n", "\r", "\t", "\<strong>", "\</strong>", "\<p>", "\</p>", "\<em>", "\</em>" ), ' ', $meta );
 
-        $meta = substr( $meta, 0, 200 );
-        $meta= str_replace('<strong>', '', $meta);
-        $meta= str_replace('</strong>', '', $meta);
-        $meta= str_replace('<p>', '', $meta);
-        $meta= str_replace('<em>', '', $meta);
-        $meta= str_replace('</em>', '', $meta);
+        $meta = substr( $meta, 0, 140 );
+        $meta= str_replace('', '', $meta);
+
 
         $keywords = get_the_category( $post->ID );
 
